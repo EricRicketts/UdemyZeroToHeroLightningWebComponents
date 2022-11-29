e6ac1153845fa3f8d26c96aa25b29f46ee0845fb
EricRicketts <eric_ricketts@me.com>
2022-11-29 17:55:17 -0500

    two way data binding

--- Section5FundamentalsOfLWC/lwcFundamentals/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ Section5FundamentalsOfLWC/lwcFundamentals/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,4 +2,9 @@ import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
   fullname = "Elmer Fudd";
+  title = "aura";
+
+  changeHandler(event) {
+    this.title = event.target.value;
+  }
 }
