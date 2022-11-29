1b494a55f9758bd79bdb45bcf2bca3edf8fdc740
EricRicketts <eric_ricketts@me.com>
2022-11-29 18:23:07 -0500

    back to using @track

--- Section5FundamentalsOfLWC/lwcFundamentals/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ Section5FundamentalsOfLWC/lwcFundamentals/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -3,7 +3,7 @@ import { LightningElement, track } from 'lwc';
 export default class HelloWorld extends LightningElement {
   fullname = "Elmer Fudd";
   title = "aura";
-  address = {
+  @track address = {
     city: "Melbourne",
     postcode: 3008,
     country: "Australia"
@@ -12,6 +12,6 @@ export default class HelloWorld extends LightningElement {
     this.title = event.target.value;
   }
   trackHandler(event) {
-    this.address = {...this.address, "city": event.target.value}; 
+    this.address.city = event.target.value;
   }
 }
