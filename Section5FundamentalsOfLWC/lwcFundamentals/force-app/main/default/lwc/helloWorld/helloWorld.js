dd1f3c82654989518eb31da6bdadce5dc034ef51
EricRicketts <eric_ricketts@me.com>
2022-11-30 10:52:20 -0500

    add setters to html and js code

--- Section5FundamentalsOfLWC/lwcFundamentals/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ Section5FundamentalsOfLWC/lwcFundamentals/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,17 +1,30 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track } from "lwc";
 
 export default class HelloWorld extends LightningElement {
   fullname = "Elmer Fudd";
   title = "aura";
-  address = {
+  @track address = {
     city: "Melbourne",
     postcode: 3008,
     country: "Australia"
-  }
+  };
+  users = ["John", "Doe", "Random"];
+  num1 = 10;
+  num2 = 20;
+
   changeHandler(event) {
     this.title = event.target.value;
   }
+
   trackHandler(event) {
-    this.address = {...this.address, "city": event.target.value}; 
+    this.address = { ...this.address, city: event.target.value };
+  }
+
+  get firstUser() {
+    return this.users[0] + " " + this.users[2] + " " + this.users[1];
+  }
+
+  get multiplyNums() {
+    return this.num1 * this.num2;
   }
 }
